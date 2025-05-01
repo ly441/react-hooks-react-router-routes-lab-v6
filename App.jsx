6d08fcada537b6500f993cebab903606bcfce9e6
Lynn <koliilynn@gmail.com>
2025-05-01 10:40:52 +0300

    fix: correct NavBar import path and restore ErrorPage component

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import NavBar from "./components/NavBar.jsx";
+import NavBar from ".src/components/NavBar.jsx";
 
 export default function App() {
   return (
